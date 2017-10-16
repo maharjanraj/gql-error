99a08c628b961862768a139033b9439443cbca7d
Raj Krishna Maharjan <raj.maharjan33@gmail.com>
2017-10-16 15:47:45 +0545

    - [x] Localize function seperated - [x] Localize made recursive - [x] each validator for validating array added

--- src/index.js
+++ src/index.js
@@ -4,18 +4,34 @@ import { locales } from './locale';
 import { has, result } from 'lodash';
 import validate from 'validate.js';
 
+// Custom validator for array
+validate.validators.each = (value, rules, key, attributes) => {
+  // Allow null and undefined values
+  if (!validate.isDefined(value)) {
+    return;
+  }
+
+  if (!validate.isArray(value)) {
+    return "must be an array";
+  }
+
+  let errors = [];
+
+  value.forEach((data, i) => {
+    let error = validate(data, rules, { fullMessages: false })
+    if (error) errors.push(error);
+  });
+
+  if (errors.length > 0) return errors;
+  else return;
+};
+
 const GQLError = {
   validate: (input, rules, options) => {
     let locale = has(options, 'lang') ? locales(options['lang']) : locales(),
       gql = has(options, 'gql') ? options.gql : true;
 
-    for (let rule in rules) {
-      for (let type in rules[rule]) {
-        if (has(rules[rule][type], 'message')) {
-          rules[rule][type]['message'] = result(locale, rules[rule][type]['message']);
-        }
-      }
-    }
+    rules = GQLError.localize(locale, rules);
 
     let errors = validate(input, rules, { fullMessages: false });
 
@@ -35,6 +51,20 @@ const GQLError = {
 
     if (gql) throw new GError(errormsg)
     else throw new Error(errormsg);
+  },
+
+  localize: (locale, rules) => {
+
+    for (let rule in rules) {
+      if (!has(rules[rule], 'message') && typeof(rules[rule] == 'object')) {
+        rules[rule] = GQLError.localize(locale, rules[rule]);
+      }
+      if (has(rules[rule], 'message')) {
+        rules[rule]['message'] = result(locale, rules[rule]['message']);
+      }
+    }
+
+    return rules;
   }
 };
 
